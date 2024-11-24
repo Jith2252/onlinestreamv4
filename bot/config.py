2b61b693e3ee413c961984ad32ac3daed9a3cb6d
Ventrapragada Naga Venkata Sivasai Prajith <125337761+Jith2252@users.noreply.github.com>
2024-11-24 15:18:58 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,20 +1,34 @@
 from os import environ as env
 
 class Telegram:
-    API_ID = int(env.get("TELEGRAM_API_ID", 12345))
-    API_HASH = env.get("TELEGRAM_API_HASH", "xyz")
-    OWNER_ID = int(env.get("OWNER_ID", 5530237028))
+    API_ID = int(env.get("TELEGRAM_API_ID", 26842016))
+    API_HASH = env.get("TELEGRAM_API_HASH", "3b841db9fa1b140e44f60cc033716f5c")
+    OWNER_ID = int(env.get("OWNER_ID", 1952992043))
     ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "").split()
-    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "BotFather")
-    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "1234567:xyz")
+    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "tmafiletolinkbot")
+    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "6896731780:AAHZo6C5u4eS6IouZrcDalM-kV0pW-t7x5g")
     BOT_WORKERS = env.get("BOT_WORKERS", 10)
-    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -100123456789))
+    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1001870465424))
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 24))
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "http://127.0.0.1:8080")
+    BASE_URL = env.get("BASE_URL", "https://tmaadda.info:8443")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
-    PORT = int(env.get("PORT", 8080))
+    PORT = int(env.get("PORT", 8443))
+    
+    #"""if 'DYNO' in environ:
+    #    ON_HEROKU = True
+     #   APP_NAME = str(getenv('APP_NAME'))
+    
+    #else:
+     #   ON_HEROKU = False
+    #ON_HEROKU = False
+    #FQDN = (getenv('FQDN', 'tmaadda.info:8443')) if not ON_HEROKU or getenv('FQDN','82.165.5.85') else APP_NAME+'.herokuapp.com'
+    #HAS_SSL=bool(getenv('HAS_SSL',True))
+    #if HAS_SSL:
+     #   URL = "https://{}/".format(FQDN)
+    #else:
+     #   URL = "http://{}/".format(FQDN)"""
 
 # LOGGING CONFIGURATION
 LOGGER_CONFIG_JSON = {
